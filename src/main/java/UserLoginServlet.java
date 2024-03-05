14882aef4e8f1394de20437ba67c7eb78182a1c8
3ak <jakneeley@outlook.com>
2024-03-04 21:19:05 -0600

    handle image upload and page styling

--- src/main/java/UserLoginServlet.java
+++ src/main/java/UserLoginServlet.java
@@ -2,14 +2,30 @@
 
 /* package net.codejava.servlet; */
  
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
- 
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,15 +36,65 @@ import java.io.IOException;
  * Servlet implementation class UserLoginServlet
  */
 @WebServlet("/login")
+@MultipartConfig(
+	fileSizeThreshold= 1024 * 1024 * 2, //2mb
+	maxFileSize = 1024 * 1024 * 10,    //10mb
+	maxRequestSize = 1024 * 1024 * 50 //50mb
+)
+
 public class UserLoginServlet extends HttpServlet {
+	
+	private static String SAVE_DIR = "uploadedFiles";
+	
+	private String extractFileName(Part part) {
+		String content = part.getHeader("content-disposition");
+		System.out.println(content);
+		String[] strItems = content.split(";");
+		for (String s : strItems) {
+			if(s.trim().startsWith("filename")) {
+				return s.substring(s.indexOf("=") + 2, s.length() -1 );
+			}
+		}
+		return "";
+	}
+	
+	private void handleImage(HttpServletRequest request,HttpServletResponse response,String fileContent) throws IOException {
+//		String URLAfterDomain = request.getRequestURI();
+//		
+//		if(URLAfterDomain.startsWith("/login/") == false) {
+//			return;
+//		}
+//		
+//		String relativePath = URLAfterDomain.substring("/images/".length());
+		
+		System.out.println(fileContent);
+		response.setContentType("image/jpeg");
+		ServletOutputStream out;
+		out = response.getOutputStream();
+		FileInputStream fin = new FileInputStream(fileContent);
+		
+		BufferedInputStream bin = new BufferedInputStream(fin);
+		BufferedOutputStream bout = new BufferedOutputStream(out);
+		
+		int ch= 0;;
+		while((ch=bin.read())!=-1) {
+			bout.write(ch);
+		}
+		
+	    bin.close();  
+	    fin.close();  
+	    bout.close();  
+	    out.close();  
+	}
+	
 	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 
 		//read forms
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
-		System.out.println("username" + username);
-		System.out.println("password" + password);
+		System.out.println("username: " + username);
+		System.out.println("password: " + password);
 		
 		//process form
 		String languages[] = request.getParameterValues("language");
@@ -37,7 +103,7 @@ public class UserLoginServlet extends HttpServlet {
 			System.out.println("Languages are: ");
 			for (String lang : languages) {
 				System.out.println("\t" + lang);
-				language += lang;
+				language += "\t" + lang;
 			}
 		}
 		
@@ -51,14 +117,37 @@ public class UserLoginServlet extends HttpServlet {
 		System.out.println("Occupation: " + occupation);
 		
 		//todo - add profile picture upload w/ upload field.
+		File f = new File("eclipse-workspace/QuickHtmlForm/src/main/webapp");
+		String appPath = f.getAbsolutePath();
+		String savePath = appPath + File.separator + SAVE_DIR;
+		
+		File fileSaveDir = new File(savePath);
+		if(!fileSaveDir.exists()) {
+			fileSaveDir.mkdir();
+		}
+		
+		Part filePart = request.getPart("file");
+		String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+		
+		try {
+			System.out.println("Upload -> " + fileSaveDir);
+			filePart.write(fileSaveDir + File.separator + extractFileName(filePart));
+			System.out.println("Upload Complete");		
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		System.out.println(fileName);
+		//handleImage(request, response, fileSaveDir + File.separator + fileName);
 		
 		//get response writer
 		PrintWriter writer = response.getWriter();
 		
 		//html
-		String htmlResponse = "<html><body>";
+		String htmlResponse = "<html><link rel=\"stylesheet\" type=\"text/css\" href=\"./style.css\"><body>";
 		htmlResponse += "<h1>User Info:</h1>";
-		htmlResponse += "<h2>"+ username + "</h2>";
+		htmlResponse += "<img src='./uploadedFiles" + fileName + "' />";
+		htmlResponse += "<h2>username: "+ username + "</h2>";
 		htmlResponse += "<h2>password: " + password + "</h2>";
 		htmlResponse += "<h2>language: " + language + "</h2>";
 		htmlResponse += "<h2>gender: " + gender + "</h2>";
